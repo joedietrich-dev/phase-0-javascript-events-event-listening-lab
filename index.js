cbc26d4491a3f908e277141ea1647d9373db2f0f
Joe Dietrich <joe@joedietrich.net>
2021-06-01 22:14:29 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+
+  const input = document.getElementById('input');
+  input.addEventListener('click', clickAlert);
 }
+
+addingEventListener();
